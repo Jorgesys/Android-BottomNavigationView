13f193f9d60745115f4c0244788a885bc3b93520
Jorgesys <jorge.osorio@gmail.com>
2017-04-30 20:30:37 -0500

    Add files via upload

---
+++ app/src/main/java/com/jorgesys/bottomnavigationview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.jorgesys.bottomnavigationview;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_favorit:
+
+                            case R.id.action_time:
+
+                            case R.id.action_muzic:
+
+                        }
+                        return true;
+                    }
+                });
+
+    }
+}
